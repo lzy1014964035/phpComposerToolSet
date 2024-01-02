d4cf6be213aa94f1dc41bec846e032958c535c3d
陆泽宇 <1014964035@qq.com>
2024-01-02 14:06:00 +0800

    【修改】 添加拼音处理包

---
+++ src/ToolSet/Service/Other/ServicePinYin.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace ToolSet\Service\Other;
+
+use Overtrue\Pinyin\Pinyin;
+use ToolSet\Service\ServiceBase;
+
+class ServicePinYin extends ServiceBase
+{
+    private $pinyinObject;
+
+    public function __construct()
+    {
+        $this->pinyinObject = new Pinyin();
+    }
+
+    // 获取拼音原始数组
+    public function getArray($string)
+    {
+        return $this->pinyinObject->convert($string);
+    }
+
+    // 获取拼音原始数组（带声调的）
+    public function getToneArray($string)
+    {
+        return $this->pinyinObject->convert($string, PINYIN_TONE);
+    }
+
+    // 获取拼音字符串
+    public function getLinkString($string, $linkSign = '')
+    {
+        return $this->pinyinObject->permalink($string, $linkSign);
+    }
+
+    // 获取拼音首字母字符串
+    public function getAbbrString($string, $linkSign)
+    {
+        return $this->pinyinObject->abbr($string, $linkSign);
+    }
+
+
+}
